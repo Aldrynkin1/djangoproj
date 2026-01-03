4ad697b9d289b2cb546d4a5ab0ebdba36782c2b6
Альберт <albertdrynkin600@gmail.com>
2026-01-03 18:32:27 +0300

    cart_app

--- kakain/kakain/settings.py
+++ kakain/kakain/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
     'cart',
 ]
 
@@ -52,6 +52,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    
+    'cart.middleware.CartMiddleware',
 ]
 
 ROOT_URLCONF = 'kakain.urls'
@@ -66,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart_processor',
             ],
         },
     },
